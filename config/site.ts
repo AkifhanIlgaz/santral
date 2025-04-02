b37f5b5a918fdea82b9a2241abdb51efd7239787
AkifhanIlgaz <akifhanilgaz@gmail.com>
2025-04-02 19:09:13 +0300

    remove announce page

--- config/site.ts
+++ config/site.ts
@@ -4,12 +4,12 @@ export const siteConfig = {
 	name: 'Ahmediye Santral',
 	navItems: [
 		{ label: 'Giriş-Çıkış', href: '/giris' },
-		{ label: 'Geçmiş', href: '/gecmis' },
-		{ label: 'Anons', href: '/anons' }
+		{ label: 'Geçmiş', href: '/gecmis' }
+		// { label: 'Anons', href: '/anons' }
 	],
 	navMenuItems: [
 		{ label: 'Giriş-Çıkış', href: '/giris' },
-		{ label: 'Geçmiş', href: '/gecmis' },
-		{ label: 'Anons', href: '/anons' }
+		{ label: 'Geçmiş', href: '/gecmis' }
+		// { label: 'Anons', href: '/anons' }
 	]
 }

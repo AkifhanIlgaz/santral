570e60eff2db31dd3ad0e66b024dccf0f2351f3e
AkifhanIlgaz <akifhanilgaz@gmail.com>
2025-04-13 08:33:00 +0300

    remove history page from navbar

--- config/site.ts
+++ config/site.ts
@@ -3,13 +3,13 @@ export type SiteConfig = typeof siteConfig
 export const siteConfig = {
 	name: 'Ahmediye Santral',
 	navItems: [
-		{ label: 'Giriş-Çıkış', href: '/giris' },
-		{ label: 'Geçmiş', href: '/gecmis' }
+		{ label: 'Giriş-Çıkış', href: '/giris' }
+		// { label: 'Geçmiş', href: '/gecmis' }
 		// { label: 'Anons', href: '/anons' }
 	],
 	navMenuItems: [
-		{ label: 'Giriş-Çıkış', href: '/giris' },
-		{ label: 'Geçmiş', href: '/gecmis' }
+		{ label: 'Giriş-Çıkış', href: '/giris' }
+		// { label: 'Geçmiş', href: '/gecmis' }
 		// { label: 'Anons', href: '/anons' }
 	]
 }
